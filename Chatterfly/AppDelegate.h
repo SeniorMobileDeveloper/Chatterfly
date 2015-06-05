0af1f29295bb04ec3f0a1ea9b0322f0e876fd3b6
PJ95 <pj95@PJ95s-iMac.local>
2015-06-05 13:49:33 +0200

    Initial Commit

---
+++ Chatterfly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chatterfly
+//
+//  Created by PJ95 on 6/5/15.
+//  Copyright (c) 2015 PJ95. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chatterfly/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chatterfly
+//
+//  Created by PJ95 on 6/5/15.
+//  Copyright (c) 2015 PJ95. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
